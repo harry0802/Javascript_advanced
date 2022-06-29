65a9f82d120072c9043104279954155a61e406b9
Harry <s6968678@gmail.com>
2022-06-29 22:25:29 +0800

    Slosure

---
+++ work/02/04_Closure/09_Example_mod copy.js
@@ -0,0 +1,13 @@
+function MYsentence1(){
+    function Saysomething(){
+        console.log('Good morning' )
+    }
+    function Dosomething(){
+        console.log('play basketball')
+    }
+    return{
+        Saysomething:Saysomething,
+        Dosomething:Dosomething
+    }
+    
+}
---
+++ work/02/04_Closure/09_Example_mod2.js
@@ -0,0 +1,15 @@
+//
+
+(function MYsentence(){
+    function Saysomething(){
+        console.log('Good morning' )
+    }
+    function Dosomething(){
+        console.log('play basketball')
+    }
+    window.myst = {
+        Saysomething:Saysomething,
+        Dosomething:Dosomething
+    }
+    
+})()
